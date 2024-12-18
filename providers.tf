a73f94767c83c7c1572ad964bc51c21c8788a76d
Haflidi Fridthjofsson <26624010+haflidif@users.noreply.github.com>
2024-12-18 15:40:01 +0100

    build(deps): Update provider versions for azurerm and azapi

--- providers.tf
+++ providers.tf
@@ -8,12 +8,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.11, < 4.0"
+      version = ">= 3.11, < 4.15"
     }
 
     azapi = {
       source  = "Azure/azapi"
-      version = ">= 1.9, < 2.0"
+      version = ">= 1.9, < 2.2"
     }
   }
 }
